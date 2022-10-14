6ed23d0a17b18d16d725d4941e773293307b3269
Fredrik <ftillman@gmail.com>
2022-10-14 15:56:06 +0200

    More tests.

--- app/src/test/java/taketwo/model/WaypointTest.java
+++ app/src/test/java/taketwo/model/WaypointTest.java
@@ -10,30 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WaypointTest {
 
-//    @Test
-//    void processBasic() {
-//        // 1 leg, first first WP @ 5sec, speeding
-//        // next wp @ 10 sec.
-//        // total time 10 sec
-//        var wp0 = new Waypoint(Timestamp.from(Instant.ofEpochSecond(someDay.getTime() + 10000)), 25.0d, 20.0d);
-//        var wp1 = new Waypoint(Timestamp.from(Instant.ofEpochSecond(someDay.getTime() + 5000)), 45.0d, 20.0d);
-//        List<Waypoint> wps = List.of(wp0, wp1);
-//        assethatrttha wp0.process(wps)
-//        System.out.println("result = " + result);
-//    }
+    private final int someTime = 1665748695; // 2022-10-14, millisecs since epoch
+
+    // Helper function to get a Timestamp at a given
+    // offset (in seconds) from the someTime reference time.
+    private Timestamp getTimestamp(long offset) {
+        return Timestamp.from(Instant.ofEpochSecond(someTime + offset));
+    }
 
     @Test
-    void superSimple() {
+    void basic() {
         // 1 leg, first first WP @ 30 sec, speeding
         // next wp @ 60 sec.
         // total time 30 sec
-        int someTime = 1665748695; // 2022-10-14, millisecs since epoch
-        final Timestamp someDay = Timestamp.from(Instant.ofEpochSecond(someTime));
-        System.out.println("someTime is " + someTime);
-        final var ts0 = Timestamp.from(Instant.ofEpochSecond(someTime + 30));
-        final var ts1 = Timestamp.from(Instant.ofEpochSecond(someTime + 60));
-        final var wp0 = new Waypoint(ts0, 25.0d, 20.0d);
-        final var wp1 = new Waypoint(ts1, 10.0d, 20.0d);
+        final var wp0 = new Waypoint(getTimestamp(30), 25.0d, 20.0d);
+        final var wp1 = new Waypoint(getTimestamp(60), 10.0d, 20.0d);
         System.out.println("wp0 is " + wp0);
         System.out.println("wp1 is " + wp1);
         assertTrue(wp0.isSpeeding());
@@ -44,5 +35,23 @@ class WaypointTest {
         System.out.println("result = " + result);
         final var expected = new State(750, 30, 750, 30);
         assertEquals(expected, result);
+    }
+
+    @Test
+    void superSonic() {
+        // 1 leg, first WP @ 0 sec, not speeding
+        // next wp @ 10 sec, speeding
+        // next @ 100 sec, not speeding
+        // last @ 110 sec, not speeding
+        // total time 30 sec
+        final var wp0 = new Waypoint(getTimestamp(0), 5d, 20d);
+        final var wp1 = new Waypoint(getTimestamp(10), 340d, 20d);
+        final var wp2 = new Waypoint(getTimestamp(100), 5d, 20d);
+        final var wp3 = new Waypoint(getTimestamp(110), 5d, 20d);
+
+        List<Waypoint> wps = List.of(wp0, wp1, wp2, wp3);
+        var result = wp0.process(wps);
+        System.out.println("result = " + result);
+        assertEquals(new State(30600, 90, 30700, 110), result);
     }
 }
